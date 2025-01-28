ed3daa13d956f47d3dcbd4fa6a506318ac368f5f
danielsm <danielsm@migdal-group.co.il>
2025-01-28 06:56:33 +0000

    first commit

--- setup.py
+++ setup.py
@@ -4,24 +4,30 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="template",
+    name="glue_sdk",
     version="0.1.0",
     author="Michael Liav",
-    author_email="liavm@migdal.co.il",
-    description="A small example package",
+    author_email="danielsm@migdal.co.il",
+    description="custom sdk for glue/spark",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://migdal.harness.io/ng/account/NmRjYjU0MzgtMTQ2MS00ZW/module/code/orgs/Platform/projects/templates/repos/migdal.platform.templates.lib.python",
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src",exclude=["main.py"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12.2",
+    python_requires=">=3.10.0",
     install_requires=[
-        # Add your dependencies here
+        'psycopg2-binary>=2.9.10',
+        'pydantic>=2.10.4',
+        'pydantic-settings>=2.7.0',
+        'dependency-injector>=4.44.0',
+        'opensearch-py>=2.7.1',
+        'redis>=5.2.1',
+        'PyYaml>=6.0.2'
     ],
     extras_require={
         'dev': [
